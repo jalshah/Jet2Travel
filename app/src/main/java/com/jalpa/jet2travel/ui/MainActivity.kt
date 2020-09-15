47b87a3c2c6dff8c45bc68982e7d5bf9ad65729f
Jalshah <jalpashah1686@gmail.com>
2020-09-15 16:58:58 +0530

    Project structure commited

---
+++ app/src/main/java/com/jalpa/jet2travel/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jalpa.jet2travel.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jalpa.jet2travel.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
